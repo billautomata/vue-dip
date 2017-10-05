a0dde567582d56ab08b8f6e7cf9dbbc9c163eb62
Bill McKessy <bill@evident.io>
2017-10-04 18:00:28 -0700

    assembler

--- test.js
+++ test.js
@@ -78,16 +78,64 @@ while(n--){
 
 probes.forEach(function(p){ console.log(p.status()) })
 
+var instruction_lut = [
+  'NOP',
+  'WRITE_MEM',
+  'WRITE_PORT',
+  'JMP_OFFSET',
+  'JMP_DIRECT',
+  'WRITE_MEM_TO_PORT',
+  'WRITE_PORT_TO_MEM',
+  'SHIFT_LEFT_MEM',
+  'SHIFT_RIGHT_MEM',
+  'INC_MEM',
+  'DEC_MEM',
+]
+
+var instructions = [
+  { fn: NOP, args: 0 },
+  { fn: WRITE_MEM, args: 2 },
+  { fn: WRITE_PORT, args: 2 },
+  { fn: JMP_OFFSET, args: 1 },
+  { fn: JMP_DIRECT, args: 1 },
+  { fn: WRITE_MEM_TO_PORT, args: 2 },
+  { fn: WRITE_PORT_TO_MEM, args: 2 },
+  { fn: SHIFT_LEFT_MEM, args: 1 },
+  { fn: SHIFT_RIGHT_MEM, args: 1 },
+  { fn: INC_MEM, args: 1 },
+  { fn: DEC_MEM, args: 1 },
+]
+
+//
+var machine_code = [0,0,0,1,1,0,0]
+var fns = []
+var idx = 0
+var instruction = {}
+while(idx < machine_code.length){
+  instruction = instructions[machine_code[idx]]
+  console.log([idx, machine_code.length, instruction_lut[machine_code[idx]],instruction.args].join('\t'))
+  if(instruction.args === 0){
+    fns.push(instruction.fn())
+  } else if (instruction.args === 1) {
+    fns.push(instruction.fn(machine_code[idx+1]))
+  } else if (instruction.args === 2) {
+    fns.push(instruction.fn(machine_code[idx+1], machine_code[idx+2]))
+  }
+  idx += instruction.args + 1
+}
+console.log(fns)
+
+
 //
 // TODO need branching
 // JMP_IF_EQUAL(_OFFSET/DIRECT)
 
 // NOP
-function NOP(){}
+function NOP(){ return function NOP(){} }
 
 // Write direct values
 function WRITE_MEM(index,value){ return function WRITE_MEM(ports,memory) { memory[index] = value } }
-function WRITE_PORT(index,value){ return function LOAD_MEM(ports,memory) { ports[index] = value } }
+function WRITE_PORT(index,value){ return function WRITE_PORT(ports,memory) { ports[index] = value } }
 
 // Jumping around
 function JMP_OFFSET(offset){ return function JMP_OFFSET(ports,memory) { memory[0] += offset-1 } }
@@ -101,4 +149,4 @@ function WRITE_PORT_TO_MEM(port_index, mem_index){ return function WRITE_PORT_TO
 function SHIFT_LEFT_MEM(mem_index){ return function SHIFT_LEFT_MEM(ports,memory) { memory[mem_index] = memory[mem_index] << 1 } }
 function SHIFT_RIGHT_MEM(mem_index){ return function SHIFT_RIGHT_MEM(ports,memory) { memory[mem_index] = memory[mem_index] >> 1 } }
 function INC_MEM(mem_index){ return function INC_MEM(ports, memory) { memory[mem_index] += 1 }  }
-function DEC_MEM(mem_index){ return function INC_MEM(ports, memory) { memory[mem_index] += 1 }  }
+function DEC_MEM(mem_index){ return function INC_MEM(ports, memory) { memory[mem_index] -= 1 }  }
